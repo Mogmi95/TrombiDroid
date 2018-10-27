d2244dca085608d6d08c830d90d5c4b16aa4571f
tbarthel-fr <thomas.barthelemy.utc@gmail.com>
2018-10-27 09:12:36 +0200

    [PersonItemView] add view skeleton
    on site session W13S2

--- app/src/main/java/apps/mogmi/fr/trombidroid/dashboard/DashboardView.kt
+++ app/src/main/java/apps/mogmi/fr/trombidroid/dashboard/DashboardView.kt
@@ -5,9 +5,10 @@ import android.support.v7.widget.LinearLayoutManager
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import android.support.v7.widget.RecyclerView
-import apps.mogmi.fr.trombidroid.data.Person
 import apps.mogmi.fr.trombidroid.R
+import apps.mogmi.fr.trombidroid.data.Person
+import kotlinx.android.synthetic.main.dashboard.view.*
+
 
 class DashboardView @JvmOverloads constructor(
         context: Context,
@@ -16,20 +17,17 @@ class DashboardView @JvmOverloads constructor(
         defStyleRes: Int = 0
 ) : LinearLayout(context, attrs, defStyle, defStyleRes), DashboardContract.View {
 
-    private var recyclerView : RecyclerView
     private var adapter: DashboardAdapter
     private var presenter: DashboardPresenter
 
     init {
         LayoutInflater.from(getContext()).inflate(R.layout.dashboard, this, true);
-
-        recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-        val manager = LinearLayoutManager(context)
-        manager.orientation = VERTICAL
-        recyclerView.layoutManager = manager
-
         adapter = DashboardAdapter()
-        recyclerView.adapter = adapter
+
+        dashboard_view_recycler_view.apply {
+            layoutManager = LinearLayoutManager(context).apply { orientation = VERTICAL }
+            adapter = this@DashboardView.adapter
+        }
 
         presenter = DashboardPresenter()
         presenter.registerView(this)
